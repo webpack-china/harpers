330bb76fef4e32f0c9eb54fd4eaf0da138b25521
lizheming <secretlzm007@gmail.com>
2017-02-12 18:18:53 +0800

    add question get api doc

--- src/api/logic/question.js
+++ src/api/logic/question.js
@@ -5,6 +5,28 @@
  * @return {}     []
  */
 export default class extends think.logic.base {
+  /**
+   * @api {GET} /question 获取问题列表
+   * @apiGroup Question
+   * @apiVersion 0.0.1
+   */
+  /**
+   * @api {GET} /question/:id 获取问题详情
+   * @apiGroup  Question
+   * @apiVersion  0.0.1
+   */
+  getAction() {
+
+  }
+  /**
+   * @api {POST} /question 新增问题
+   * @apiGroup Question
+   * @apiVersion 0.0.1
+   * 
+   * @apiParam  {String}  title 问题标题
+   * @apiParam  {String[]}  tag 问题标签
+   * @apiParam  {String}  markdown_content 问题内容
+   */
   postAction() {
     this.rules = {
       title: 'string|required',
