6e72d00970012d1e8ae92b2d435d728d98c3959a
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2017-04-02 20:39:59 +0800

    update package.json

--- www/web/tools/template/index/main.vue
+++ www/web/tools/template/index/main.vue
@@ -1,23 +1,3 @@
-<style lang="scss" rel="stylesheet/scss">
-    @import "css/main";
-    @import "css/main";
-
-    .container {
-        font-size: 30px;
-
-        .steamer {
-            color: aqua;
-            span {
-                color: red;
-            }
-        }
-    }
-</style>
-
-<style lang="styl">
-    @import "css/main.styl";
-</style>
-
 <template>
     <div class="container">
         <p class="steamer">Hello <span>{{name}}</span></p>
