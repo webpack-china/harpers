aec471ddfbfef42e574a9bf58e3f7bfa50e75601
huangtubiao <1334615045@qq.com>
2017-04-02 19:00:28 +0800

    add router

---
+++ www/web/src/routers.js
@@ -0,0 +1,10 @@
+const routers = [{
+    path: '/',
+    name: 'index',
+    component: resolve => require(['page/index_index/main.vue'], resolve)
+}, {
+    path: '*',
+    component: resolve => require(['page/index_index/main.vue'], resolve)
+}]
+
+export default routers
