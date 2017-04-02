faf859e348482d59245c9955548f1863b3a5b734
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2017-04-02 20:41:21 +0800

    add semi colon

--- www/web/src/page/index_index/main.js
+++ www/web/src/page/index_index/main.js
@@ -1,18 +1,18 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import routers from 'routers'
-import Main from './main.vue'
-import './css/main.less'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import routers from 'routers';
+import Main from './main.vue';
+import './css/main.less';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const router = new VueRouter({
     mode: 'history',
     routers
-})
+});
 
 new Vue({
     el: '#main',
     router,
     render: h => h(Main)
-})
+});
