5e05729b8198a5593efe2064d827a49b273016fe
lizhihua <275091674@qq.com>
2017-04-06 21:57:58 +0800

    develop detail page

--- www/web/src/page/detail/detail.vue
+++ www/web/src/page/detail/detail.vue
@@ -1,13 +1,56 @@
 <template>
-  <div class="detail">
-      Detail
-      <list v-for="item in items" :name="item.name" :key="item.key"></list>
+  <div class="list-page">
+    <ul class="navbar clearfix">
+      <li class="navbar-li" :class="title.isActive ? 'active' : ''" v-for="(title, index) in titles"
+          @click="switchContent(index, title)">{{title.name}}
+
+      </li>
+    </ul>
+    <list v-for="item in items" :name="item.name" :key="item.key"></list>
   </div>
 </template>
+
+<style lang="less">
+  .list-page {
+    background-color: #fff;
+  }
+
+  .navbar {
+    margin: 20px 20px 0;
+    border-bottom: 1px solid #D8D8D8;
+
+  }
+
+  .navbar-li {
+    float: left;
+    font-size: 14px;
+    margin-right: 30px;
+    cursor: pointer;
+    line-height: 34px;
+    color: #999;
+    &.active {
+      color: #333;
+    }
+  }
+</style>
+
 
 <script>
 
     import List from './list';
+
+    var titles = [{
+        name: '最新动态'
+    }, {
+        name: '新问答'
+    }, {
+        name: '热门问答'
+    }, {
+        name: '等待回答'
+    }];
+    titles.forEach(function (item, index) {
+        item.isActive = index === 0;
+    });
 
     export default {
 
@@ -17,8 +60,19 @@
             list: List
         },
 
+        methods: {
+            switchContent: function (index, titleObj) {
+                this.titles.forEach(function (item) {
+                    item.isActive = false;
+                });
+                titleObj.isActive = true;
+            }
+        },
+
         data () {
             return {
+                titles,
+
                 items: [{
                     key: 100,
                     name: 1
@@ -34,6 +88,3 @@
     };
 
 </script>
-
-<style lang="less">
-</style>
--- www/web/src/page/index_index/main.vue
+++ www/web/src/page/index_index/main.vue
@@ -5,13 +5,29 @@
 
     body {
         background-color: #2B3A42;
+        margin: 0;
     }
     a {
         color: #fff;
     }
+    ul, li{
+        margin: 0;
+        padding: 0;
+    }
+    li {
+        list-style: none;
+    }
     .container {
         font-size: 30px;
         min-height: 320px;
+    }
+    .clearfix:before,.clearfix:after{
+        content:"";
+        display:table;
+    }
+    .clearfix:after{clear:both;}
+    .clearfix{
+        *zoom:1;/*IE/7/6*/
     }
 </style>
 
