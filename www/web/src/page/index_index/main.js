50c15a2474e1396fbeb9f40d746bcf8fdb645929
zd98 <zhendong9860@163.com>
2017-04-06 10:35:47 +0800

    add vuex & store

--- www/web/src/page/index_index/main.js
+++ www/web/src/page/index_index/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import router from '../../router';
 import Main from './main.vue';
+import store from '../../store';
 import './css/main.less';
 
 // 页面主视图，router的容器
 new Vue({
     el: '#main',
     router,
+    store,
     render: h => h(Main)
 });
