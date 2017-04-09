aab789db981810a3275434f781a5ed8e098af763
huangtubiao <1334615045@qq.com>
2017-04-09 23:33:05 +0800

    add notes

--- www/web/src/components/search/index.js
+++ www/web/src/components/search/index.js
@@ -1,3 +1,12 @@
+/*
+* @Author: leo
+* @Date:   2017-04-08 23:19:00
+* @Last Modified by:   leo
+* @Last Modified time: 2017-04-08 23:19:00
+*/
+
+'use strict';
+
 import Search from './search.vue';
 
 export default Search;
--- www/web/src/page/home/index.js
+++ www/web/src/page/home/index.js
@@ -1,3 +1,12 @@
+/*
+* @Author: leo
+* @Date:   2017-04-08 23:19:00
+* @Last Modified by:   leo
+* @Last Modified time: 2017-04-08 23:19:00
+*/
+
+'use strict';
+
 import Home from './home.vue';
 
 export default Home;
