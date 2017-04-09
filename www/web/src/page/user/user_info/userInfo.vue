fd26ce8eeecc6e531d221ab05a1d8a5859bf1a5b
zd98 <zhendong9860@163.com>
2017-04-09 20:43:20 +0800

    user_info user page private

---
+++ www/web/src/page/user/user_info/userInfo.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="h-user-info">
+   <div>
+   		<div></div>
+   		<div>
+   			<div>
+   				<p></p>
+   				<p></p>
+   			</div>
+   			<div>
+   				<ul>
+   					<li>
+   						<p></p>
+   						<p></p>
+   					</li>
+   					<li>
+   						<p></p>
+   						<p></p>
+   					</li>
+   					<li>
+   						<p></p>
+   						<p></p>
+   					</li>
+   				</ul>
+   			</div>
+   			<div></div>
+   		</div>
+   </div> 
+</div>
+</template>
+
+<script>
+export default {
+
+  name: 'UserInfo',
+
+  data () {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style lang="less">
+</style>
